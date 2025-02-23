157c16def179fd6a7c31a546109ca570a757cf73
taofeeq2014 <taofeeq2014@outlook.com>
2025-02-23 21:13:11 +0100

    Update UserContext.js

--- alx-react-app-props/src/components/UserContext.js
+++ alx-react-app-props/src/components/UserContext.js
@@ -1,1 +1,7 @@
 function React.createContext()
+import UserContext from './UserContext';
+function UserContext.Provider ({ UserContext }) {
+  return <UserContext UserContext={UserContext} />;
+}
+
+export default UserContext;
